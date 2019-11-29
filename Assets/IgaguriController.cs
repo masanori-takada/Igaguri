a2067760ce404f40a59f4aa80c526b40170a8a48
高田将典 <high_field_general_06217@yahoo.co.jp>
2019-11-29 23:14:31 +0900

    no message

---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+        //Shootは関数。他のスクリプトからアクセスするからpublic修飾子。
+        //関数内でVector3型の、変数dirを仮引数として代入する。
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+        //Rigidbody特有の機能を使えるようにして、
+        //その中のAddForceを使用する。引数に入れる下記のVector3の方向に力を加える。
+    }
+
+    void OnCollisionEnter(Collision other)
+        //OnCollisionEnterは、当たったタイミングでスクリプトの内容が呼ばれる関数。
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        //kinematicは運動という意味だが、これがtrueになることで物理影響を受けなくなる
+        GetComponent<ParticleSystem>().Play();
+        //ParticleSystemの機能を使えるようにして、その中のPlayメソッドを使用する
+    }
+
+    void Start()
+    {
+        //Shoot(new Vector3(0, 200, 2000));
+        //上記の関数を使っている。引数の方向に力を加える。
+        //Y方向に200の力が加わっているのは、的に届くまでに重力で落下するのを防ぐため
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/IgaguriGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+    public GameObject igaguriPrefab;
+    //igaguriPrefabの箱を用意しておく。ここではまだ箱だけ
+    //アウトレット接続により、スクリプト内の変数にオブジェクト（igaguriPrefab)の実態を代入する
+    //スクリプト側にコンセントの差し込み口をつくるために、変数の前にpublic修飾子をつける
+    //これにより、インスペクタから変数が見えるようになり、代入したいオブジェクト（igaguriPrefab）をアタッチできる
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab) as GameObject;
+            //Instantiateは、クローンのオブジェクトを生成するメソッド
+            //Instantiateメソッドは、引数に設計図を渡すと、戻り値としてインスタンスを返す
+            //GameObject型の変数igaguriを定義して、上記でできたインスタンスを代入している
+            //as GameObjectは、キャストという強制的な型変換。
+            //InstantiateはObject型で返すのでそれをGameObject型にしている
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //ScreenPointToRayは、カメラからタップ座標に向かうベクトルに沿って進むRay（光線）を返すメソッド
+            Vector3 worldDir = ray.direction;
+            //Rayクラスは、光源の座標（origin）と、光線の方向（direction）、をメンバ変数に持っている
+            //directionは、カメラからタップした座標に向かうベクトル
+            igaguri.GetComponent<IgaguriController>().Shoot(worldDir.normalized * 2000);
+            //生成したオブジェクトに対して動きを付与している
+            //IgaguriControllerのスクリプトを取得し、その関数のShootメソッドを使用している
+            //normalizedは、worlddirを長さ１の単位ベクトルに変換した後、一定の力を加えるため2000倍している
+
+        }
+    }
+}
